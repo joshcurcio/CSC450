80b48c12034552564e4350f3abf9bf1a99d48054
Josh Curcio <pennwrestler31@gmail.com>
2016-02-15 00:37:05 +0000

    assignment 4

---
+++ copyReverse.c
@@ -0,0 +1,37 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "tlpi_hdr.h"
+#ifndef BUF_SIZE
+#define BUF_SIZE 1024
+#endif
+/* Allow "cc -D" to override definition */
+ 
+void main(int argc, char *argv[])
+{
+    int count;
+    char ch, ch1;
+    FILE *fp1, *fp2;
+ 
+    if (fp1 = fopen("input.txt", "r"))    
+    {
+        fp2 = fopen("output.txt", "w");
+        count = count_characters(fp1); 
+        fseek(fp1, -1L, 2);
+ 
+        while (count)
+        {
+            ch = fgetc(fp1);
+            fputc(ch, fp2);
+            fseek(fp1, -2L, 1);
+            count--;
+        }
+    }
+}
+
+int count_characters(FILE *f) 
+{
+    fseek(f, -1L, 2);
+    int last_pos = ftell(f);
+    last_pos++;
+    return last_pos;
+}
