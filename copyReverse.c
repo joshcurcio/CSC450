087d526d5e59ab26312f72066c6c1b85cd6191e8
Josh Curcio <pennwrestler31@gmail.com>
2016-02-15 17:33:46 +0000

    assignment 4

--- copyReverse.c
+++ copyReverse.c
@@ -15,14 +15,14 @@ void main(int argc, char *argv[])
     if (inputFp = fopen(argv[1], "r"))    
     {
         outputFp = fopen(argv[2], "w+");
-        count = count_characters(fp1); 
-        fseek(inputFp, -1L, 2);
+        count = count_characters(inputFp); 
+        fseek(inputFp, -1, 2); //2 takes it to the end of the file -1 takes it to the last char
  
         while (count)
         {
             currChar = fgetc(inputFp);
             fputc(currChar, outputFp);
-            fseek(inputFp, -2L, 1);
+            fseek(inputFp, -2, 1); //1 takes it to the current pointer and -2 takes it to the char before it
             count--;
         }
     }
